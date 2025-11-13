d736ae433fde0be417f2c814a41334a1ac54e297
Tohid4641 <dev.tauhid@gmail.com>
2025-11-13 23:19:22 +0530

    cors changes

--- src/app.js
+++ src/app.js
@@ -27,7 +27,7 @@ const BASE_URL = NODE_ENV === 'production'
 //   credentials: true
 // }));
 
-app.use(cors());
+app.use(cors('*'));
 
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true, limit: '10mb' }));
