fc55cef4deeee1cf290ed96c6b3049a63222fc3b
Tohid4641 <dev.tauhid@gmail.com>
2025-11-14 16:59:15 +0530

    cors changes

--- src/utils/cors.js
+++ src/utils/cors.js
@@ -2,11 +2,8 @@ import cors from 'cors';
 
 export default function corsConfig() {
     return cors({
-        origin: [
-            'http://localhost:5174',
-            'http://localhost:5173',
-            'https://poultry-record-frontend.vercel.app',
-        ],
-        credentials: true,
+        origin: "*",
+        // credentials: true,
+        methods: ['GET', 'POST', 'PUT', 'DELETE', "PATCH",],
     });
 }
