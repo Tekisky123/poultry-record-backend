fcca7ca48dfdfff9c5291dd2143fc2f6e0bcaccb
Tohid4641 <dev.tauhid@gmail.com>
2025-11-14 17:32:50 +0530

    cors changes

--- src/app.js
+++ src/app.js
@@ -19,9 +19,6 @@ const BASE_URL = NODE_ENV === 'production'
 // ✅ CORS must be FIRST middleware
 app.use(corsConfig());
 
-// Handle preflight requests explicitly (for Vercel)
-app.options('*', corsConfig());
-
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true, limit: '10mb' }));
 app.use(cookieParser());
--- src/utils/cors.js
+++ src/utils/cors.js
@@ -1,9 +1,21 @@
 import cors from 'cors';
 
+const allowedOrigins = [
+    'http://localhost:5174',
+    'http://localhost:5173',
+    'https://poultry-record-frontend.vercel.app',
+];
+
 export default function corsConfig() {
     return cors({
-        origin: "*",
-        // credentials: true,
-        methods: ['GET', 'POST', 'PUT', 'DELETE', "PATCH",],
+        origin: (origin, callback) => {
+            if (allowedOrigins.includes(origin)) {
+                callback(null, true);
+            } else {
+                callback(new Error('Not allowed by CORS'));
+            }
+        },
+        methods: ['GET', 'POST', 'PUT', 'DELETE', "PATCH", "OPTIONS"],
+        credentials: true,
     });
 }
