fd54df40f2a0b7b1d3e9af6698981d7637a30de1
Tohid4641 <dev.tauhid@gmail.com>
2026-01-03 14:45:47 +0530

    db change to QA

--- src/configs/database.js
+++ src/configs/database.js
@@ -1,10 +1,10 @@
 import mongoose from 'mongoose';
-import {config} from 'dotenv';
+import { config } from 'dotenv';
 
 config({ path: `${process.cwd()}/src/.env` });
 
 const connectDB = async () =>
-  await mongoose.connect(`${process.env.DATABASE_URI}/poultryRecordDB`, {
+  await mongoose.connect(`${process.env.DATABASE_URI}/poultryRecordDBQA`, {
     serverSelectionTimeoutMS: 5000,
     socketTimeoutMS: 45000,
   });
