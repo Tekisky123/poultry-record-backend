b8de0fc49e565aa9548da920478978c36da35bd3
Tohid4641 <dev.tauhid@gmail.com>
2026-01-13 20:50:04 +0530

    cors changes2

--- src/app.js
+++ src/app.js
@@ -16,18 +16,14 @@ const BASE_URL = NODE_ENV === 'production'
   ? 'https://poultry-record-backend-qa.vercel.app/api'
   : `http://localhost:${port}`;
 
-
-
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true, limit: '10mb' }));
 app.use(cookieParser());
 app.use(apiLogger);
 
 // app.use(corsConfig());
-app.use(cors({
-  origin: '*',
-  methods: ['GET', 'POST', 'PUT', 'DELETE', "PATCH"],
-}));
+app.use(cors());
+
 const server = http.createServer(app);
 
 app.use("/api", router);
