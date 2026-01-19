d84bb2dfa762acdf02afdba9d9dc03699c4b7820
Tohid4641 <dev.tauhid@gmail.com>
2026-01-19 23:00:38 +0530

    cors changes3

--- src/app.js
+++ src/app.js
@@ -20,12 +20,7 @@ const BASE_URL = NODE_ENV === 'production'
 
 app.use(
   cors({
-    origin: [
-    "http://localhost:5173",
-    "http://localhost:5174",
-    "https://poultry-record-frontend.vercel.app",
-    "https://poultry-record-frontend-qa.vercel.app",
-  ],
+    origin: "https://poultry-record-frontend-qa.vercel.app",
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
     credentials: true,
   })
