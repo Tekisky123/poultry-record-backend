2d210c103f93db50c4e1860cc374751c96a16ceb
Tohid4641 <dev.tauhid@gmail.com>
2026-01-19 22:57:05 +0530

    cors changes2

--- src/app.js
+++ src/app.js
@@ -16,7 +16,15 @@ const BASE_URL = NODE_ENV === 'production'
   : `http://localhost:${port}`;
 
 // ✅ CORS must be FIRST middleware
-app.use(corsConfig());
+// app.use(corsConfig());
+
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
+    credentials: true,
+  })
+)
 
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true, limit: '10mb' }));
