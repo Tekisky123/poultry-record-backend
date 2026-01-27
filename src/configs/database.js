fe6d4b761344e433ee5fe8dcd073d2e7aed13ba3
Tohid4641 <dev.tauhid@gmail.com>
2026-01-27 13:08:25 +0530

    some env's changes

--- src/configs/database.js
+++ src/configs/database.js
@@ -1,10 +1,10 @@
 import mongoose from 'mongoose';
-import {config} from 'dotenv';
+import { config } from 'dotenv';
 
 config({ path: `${process.cwd()}/src/.env` });
 
 const connectDB = async () =>
-  await mongoose.connect(`${process.env.DATABASE_URI}/poultryRecordDB`, {
+  await mongoose.connect(`${process.env.DATABASE_URI}/${process.env.DATABASE_NAME}`, {
     serverSelectionTimeoutMS: 5000,
     socketTimeoutMS: 45000,
   });
